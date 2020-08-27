d8ad70c0b9862b0960191f43a5a59b7f43659d0f
Alessandro <sandromuniz27@hotmail.com>
2020-08-27 14:51:27 -0400

    feat: add listar classes e criar conexões

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -10,47 +10,8 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
-  async index(req: Request, res: Response) {
-    const filters = req.query;
-
-    const subject = filters.subject as string;
-    const week_day = filters.week_day as string;
-    const time = filters.time as string;
-
-    if (!filters.week_day || !filters.subject || !filters.time) {
-      return res.status(400).json({
-        error: 'Missing filters to search classes',
-      });
-    }
-
-    const timeInMinutes = convertHourToMinutes(time);
-
-    const classes = await db('classes')
-      .whereExists(function () {
-        this.select('class_schedule.*')
-          .from('class_schedule')
-          .whereRaw('`class_schedule`.`class_id`')
-          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
-      })
-      .where('classes.subject', '=', subject)
-      .join('users', 'classes.user_id', '=', 'users.id')
-      .select(['classes.*', 'users.*']);
-
-      return res.json(classes);
-  }
-
-  async create(req: Request, res: Response) {
-    const {
-      name,
-      avatar,
-      whatsapp,
-      bio,
-      subject,
-      cost,
-      schedule,
-    } = req.body;
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
 
     const trx = await db.transaction();
 
@@ -61,6 +22,7 @@ export default class ClassesController {
         whatsapp,
         bio,
       });
+
       const user_id = insertedUsersIds[0];
 
       const insertedClassesId = await trx('classes').insert({
@@ -68,24 +30,60 @@ export default class ClassesController {
         cost,
         user_id,
       });
+
       const class_id = insertedClassesId[0];
 
-      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => ({
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinutes(scheduleItem.from),
-        to: convertHourToMinutes(scheduleItem.to),
-      }));
+      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+          class_id,
+          week_day: scheduleItem.week_day,
+          from: convertHourToMinutes(scheduleItem.from),
+          to: convertHourToMinutes(scheduleItem.to),
+        };
+      });
 
       await trx('class_schedule').insert(classSchedule);
 
       await trx.commit();
 
-      return res.status(201).send();
+      return response.status(201).send();
     } catch (err) {
       await trx.rollback();
-
-      return res.status(400).json({ error: 'Unexpected error while creating new class.' });
+      return response.status(400).json({
+        error: 'Unexpected error while creating new class.'
+      });
     }
   }
+
+  async index(request: Request, response: Response): Promise<Response> {
+    const filters = request.query;
+
+    const subject = filters.subject as string;
+    const week_day = filters.week_day as string;
+    const time = filters.time as string;
+
+    if (!filters.week_day || !filters.subject || !filters.time) {
+      return response.status(400).json({
+        error: 'Missing filters to search classes',
+      });
+    }
+
+    const timeInMinutes = convertHourToMinutes(time);
+
+    const classes = await db('classes')
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*']);
+
+      return response.json(classes);
+  }
+
 }
