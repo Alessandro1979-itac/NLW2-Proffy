293c2766b252bb1ed3fa0b09237859a987c208c6
Alessandro <sandromuniz27@hotmail.com>
2020-08-27 14:07:37 -0400

    feat: add select e componente textArea

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -11,19 +11,15 @@ interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   }>;
 }
 
-const Select: React.FC<SelectProps> = ({ name, label, options, ...rest }) => {
+const Select: React.FC<SelectProps> = ({ name, label, options, ...rest }: SelectProps) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
       <select id={name} {...rest}>
-        <option value="" disabled selected hidden>
-          Selecione uma opção
-        </option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
+        <option value="" disabled selected hidden>Selecione uma opção</option>
+        {options.map(option => {
+          return <option key={option.value} value={option.value}>{option.label}</option>;
+        })}
       </select>
     </div>
   );
