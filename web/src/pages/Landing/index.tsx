2e3016454da0af23f69c210719bd329cf48de19e
Alessandro <sandromuniz27@hotmail.com>
2020-08-27 14:10:07 -0400

    feat: add página incial responsiva

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
+
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
-export default function Landing() {
+export default function Landing(): ReactElement {
+  const [totalConnections, settotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      settotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -18,10 +30,7 @@ export default function Landing() {
           <h2>Sua plataforma de estudos online.</h2>
         </div>
         <img
-          src={landingImg}
-          alt="plataforma de estudos"
-          className="hero-image"
-        />
+          src={landingImg} alt="plataforma de estudos" className="hero-image" />
 
         <div className="buttons-container">
           <Link to="/study" className="study">
@@ -36,7 +45,11 @@ export default function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas.{" "}
+          Total de
+          {' '}
+          {totalConnections}
+          {' '}
+          conexões já realizadas.
           <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
       </div>
